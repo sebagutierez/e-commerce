4baf82e5fbb3704dd26ccbb0a16692172c9720ca
Gonzalo Dinoia <gonzalodinoia@gmail.com>
2022-08-24 16:54:51 -0300

    correcciones navbar

--- src/components/Navbar/BtnCart.js
+++ src/components/Navbar/BtnCart.js
@@ -1,6 +1,6 @@
 const BtnCart = () => {
     return (
-      <div className="flex justify-center align-middle p-1 ml-8 text-3xl w-10 h-10 rounded-full font-bold cursor-pointer border-2 border-solid border-black "><ion-icon name="cart-outline"></ion-icon></div>
+      <div className="flex justify-center w-10 h-10 p-1 ml-8 text-3xl font-bold text-gray-800 align-middle duration-500 border-2 border-gray-800 border-solid rounded-full cursor-pointer hover:text-gray-400 hover:border-gray-400 "><ion-icon name="cart-outline"></ion-icon></div>
     )
   }  
   export default BtnCart
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,33 +18,28 @@ const Navbar = () => {
     let [open,setOpen]=useState(false);
     return (
    
-    <div className='bg-white shadow-md w-full fixed top-0 left-0 z-50'>
-        <div className='md:flex items-center justify-between bg-gray py-4 md: px-10'>
-            <div className='font-ftitles font-bold text-xl cursor-pointer flex items-center text-gray-800'>
-                <span className='text-3x1 text-indigo-600 mr-1 pt-2'>
+    <div className='fixed top-0 left-0 z-50 w-full bg-white shadow-md'>
+        <div className='items-center justify-between px-10 py-4 md:flex bg-gray md:'>
+            <div className='flex items-center text-xl font-bold text-gray-800 duration-500 cursor-pointer hover:text-gray-400 font-ftitles'>
+                <span className='pt-2 mr-1 text-indigo-600 text-3x1'>
                 <ion-icon name='book'></ion-icon>
                 </span>
                 TIENDA DE LIBROS
             </div>
-            <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+            <div onClick={()=>setOpen(!open)} className='absolute text-3xl text-gray-800 duration-500 cursor-pointer hover:text-gray-400 right-8 top-6 md:hidden'>
             <ion-icon name={open ? 'close':'menu'}></ion-icon>
             </div>
             <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-1 left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-18 opacity-100':'top-[-490px]'} md:opacity-100`}>
                 {
                     links.map((link)=>(
-                        <li key={link.name} className='font-ftitles md: ml-8 text-xl md:my-0 my-7'>
-                            <a href={link.link}>{link.name}</a>
+                        <li key={link.name} className='ml-8 text-xl font-ftitles md: md:my-0 my-7'>
+                            <a href={link.link} className='text-gray-800 duration-500 hover:text-gray-400'>{link.name}</a>
                         </li>
-
-                    ))
-                }
+                    ))                }
                 <BtnCart/>
             </ul>
-
-
         </div>
     </div>
   )
 }
-
 export default Navbar
