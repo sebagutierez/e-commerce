5342d498a686bd6c695f993b89845f4cd0648f0a
Gonzalo Dinoia <gonzalodinoia@gmail.com>
2022-08-29 18:32:39 -0300

    creacion cards

--- src/components/Cards/CardContainer.js
+++ src/components/Cards/CardContainer.js
@@ -3,7 +3,9 @@ import databooks from "./databooks";
 
 const CardContainer = () => {
   return (
-    <div className="flex flex-wrap justify-center mt-24">
+    <>
+    <h2 className="text-center mt-24 mb-8 font-ftitles text-2xl text-gray-800">PRODUCTOS</h2>
+    <div className="flex flex-wrap justify-center">
   {databooks.map((e) => {
     let imglibro;
     if(e.stock < 10 ){
@@ -12,10 +14,10 @@ const CardContainer = () => {
         imglibro=e.img;
       }
        return (
-       <Cards title={e.title} img={imglibro} author={e.author} price={e.price}/>
-     );})}
-            
+       <Cards id={e.id} title={e.title} img={imglibro} author={e.author} price={e.price}/>
+     );})}           
   </div>
+  </>
   )
 }
 
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,5 +1,3 @@
-import CardsButton from "./CardsButton";
-
 const Cards = ({img,title,author,price}) => { //con destructuring podria pasarle {(img,title,text)} y abajo invocar sin usar props.
     return (
             <div className="w-72 h-auto p-4 m-1.5 rounded-lg overflow-hidden bg-white transition-all duration-500 shadow-md cursor-pointer hover:shadow-lg hover:scale-110 hover:transtion-all ease-in-out">
@@ -8,7 +6,9 @@ const Cards = ({img,title,author,price}) => { //con destructuring podria pasarle
                     <h2 className="text-xl font-bold text-center font-ftitles">{title}</h2>
                     <p className="text-center font-ftext">Autor: {author}</p>
                     <p className="font-bold text-center font-ftext">${price}</p>
-                    <CardsButton/>
+                    <button className=" text-sm font-ftext my-2.5 p-2 w-36 font-bold cursor-pointer rounded-xl bg-transparent border-solid border-2 border-black hover:bg-black hover:text-white transition-all duration-600 ease-in">
+          Agregar al Carrito
+      </button>
                 </div>
             </div>
     );
